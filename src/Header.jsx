684b7ce6d372013f6280612756a8a6f76c2171c5
itsbamme <jan9239@naver.com>
2025-05-10 15:53:20 +0900

    feat: GlobalProvider에서 useReducer가 반환하는 변경된 전역 상태와 전역상태 변경함수를 하위 컴포넌트에 전달

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,8 @@ import { GlobalContext } from './GlobalProvider';
 
 export default function Header() {
 	//자식 컴포넌트에서는 useContext로 전역 컨텍스트값 호출 가능
-	const data = useContext(GlobalContext);
+	const { GlobalState, dispatch } = useContext(GlobalContext);
+	console.log(GlobalState);
 	return (
 		<header>
 			<h1>header</h1>
